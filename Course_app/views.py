e16c645c8d2b3bb8c5c23653290d36fa95f52806
Dev22Patel <156922885+Dev22Patel@users.noreply.github.com>
2024-04-03 21:45:29 +0530

    deleted some unused code in python

--- Course_app/views.py
+++ Course_app/views.py
@@ -24,29 +24,29 @@ def course_form(request):
     return render(request,"form.html")
 
 
-def serve_file(request, file_name):
-    # Retrieve the model instance using the file name
-    try:
-        instance = Course.objects.get(file_field=file_name)
-    except Course.DoesNotExist:
-        return HttpResponse("File not found", status=404)
+# def serve_file(request, file_name):
+#     # Retrieve the model instance using the file name
+#     try:
+#         instance = Course.objects.get(file_field=file_name)
+#     except Course.DoesNotExist:
+#         return HttpResponse("File not found", status=404)
     
-    # Retrieve the file content
-    file_content = instance.file_field.read()
+#     # Retrieve the file content
+#     file_content = instance.file_field.read()
     
-    # Determine the file's content type
-    content_type = 'application/octet-stream'  # Default content type
-    if instance.file_field.name.endswith('.pdf'):
-        content_type = 'application/pdf'
-    elif instance.file_field.name.endswith('.jpg'):
-        content_type = 'image/jpeg'
-    # Add more conditions for other file types as needed
+#     # Determine the file's content type
+#     content_type = 'application/octet-stream'  # Default content type
+#     if instance.file_field.name.endswith('.pdf'):
+#         content_type = 'application/pdf'
+#     elif instance.file_field.name.endswith('.jpg'):
+#         content_type = 'image/jpeg'
+#     # Add more conditions for other file types as needed
     
-    # Create an HttpResponse object with the file content and appropriate content type
-    response = HttpResponse(file_content, content_type=content_type)
+#     # Create an HttpResponse object with the file content and appropriate content type
+#     response = HttpResponse(file_content, content_type=content_type)
     
-    # Set the Content-Disposition header to force download or inline display
-    response['Content-Disposition'] = f'attachment; filename="{file_name}"'  # Force download
-    # response['Content-Disposition'] = f'inline; filename="{file_name}"'  # Inline display
+#     # Set the Content-Disposition header to force download or inline display
+#     response['Content-Disposition'] = f'attachment; filename="{file_name}"'  # Force download
+#     # response['Content-Disposition'] = f'inline; filename="{file_name}"'  # Inline display
     
-    return response
+#     return response
